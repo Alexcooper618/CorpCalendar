6268ba7dc6d08866221ec12bb4b127c217de41d6
Alexcooper618 <dr.nagimov30@gmail.com>
2025-12-12 15:23:27 +0300

    Remove ValidationPipe to fix class-transformer runtime error

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,21 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   app.setGlobalPrefix('api');
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidUnknownValues: false,
-      transform: true,
-    }),
-  );
 
   const port = process.env.PORT || 4000;
   await app.listen(port, '0.0.0.0');
+
   console.log(`Backend running on http://localhost:${port}/api`);
 }
 bootstrap();
